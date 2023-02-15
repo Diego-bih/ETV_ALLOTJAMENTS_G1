efd6462dd584d7905411636bb77faf2eefe9162e
IvanTZ <zaychev010@gmail.com>
2023-02-15 17:47:21 +0100

    1.1

--- renders/renderhotel.js
+++ renders/renderhotel.js
@@ -6,5 +6,12 @@ function verAlojamiento(id){
     ipcRenderer.send("request1",(e,id))
 }
 ipcRenderer.on("request-res",(e,args)=>{
-    
+    const respuesta = JSON.parse(args);
+        $("#img")= <img src=args.img alt=''>
+        //+`<div class="info">`
+        //+`<p>Nom: ${element.nom}</p>`
+        //+`<p>Municipi: ${element.municipi_id}</p>`
+        //+`<p>Pax: ${element.npersones}</p>`
+        //+`<p>Valoracio: ${element.valoracio}</p>`
+        //+`<textarea cols="30" rows="5"> ${element.descripcio}</textarea></div></div>`
 })
